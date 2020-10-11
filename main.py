91d1021062f61b964ea9a1300630c59c462f8124
ShriramShagri <shagrisrm@gmail.com>
2020-10-11 15:57:24 +0530

    Basic player setup

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from tkinter import *
+from pygame import mixer
 
 root = Tk()
+
+mixer.init()
+
+root.geometry('600x300')
+root.title("Melody")
+root.iconbitmap(r'./assets/favicon.ico')
+
+text = Label(root, text="Let's make something nice").pack()
+
+photo = PhotoImage(file="./assets/play.png")
+# labelphoto = Label(root,image=photo).pack()
+
+def playMusic():
+    mixer.music.load('./assets/Testmusic/AlanWalker.mp3')
+    mixer.music.play()
+
+play_btn = Button(root, image=photo, command=playMusic).pack()
+
+
 
 root.mainloop()
